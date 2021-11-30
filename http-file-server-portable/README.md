9967f3651c3aa6ec0b41156e828de8288aed220b
Ryan Restivo <53523282+ryanmrestivo@users.noreply.github.com>
2021-11-30 14:50:19 -0500

    http file server initial commit

---
+++ http-file-server-portable/README.md
@@ -0,0 +1,138 @@
+# http-file-server
+
+`http-file-server` is a dependency-free HTTP file server. Beyond directory listings and file downloads, it lets you download a whole directory as as `.zip` or `.tar.gz` (generated on-the-fly).
+
+![screenshot](doc/screenshot.png)
+
+## Contents
+
+- [Contents](#contents)
+- [Examples](#examples)
+  - [Serving a path at `/`](#serving-a-path-at-)
+  - [Serving $PWD at `/`](#serving-pwd-at-)
+  - [Serving multiple paths, setting the HTTP port via CLI arguments](#serving-multiple-paths-setting-the-http-port-via-cli-arguments)
+  - [Setting the HTTP port via environment variables](#setting-the-http-port-via-environment-variables)
+  - [Uploading files using cURL](#uploading-files-using-curl)
+  - [HTTPS (SSL/TLS)](#https-ssltls)
+- [Get it](#get-it)
+  - [Using `go get`](#using-go-get)
+  - [Pre-built binary](#pre-built-binary)
+- [Use it](#use-it)
+
+## Examples
+
+### Serving a path at `/`
+
+```sh
+$ http-file-server /tmp
+2018/11/13 23:00:03 serving local path "/tmp" on "/tmp/"
+2018/11/13 23:00:03 redirecting to "/tmp/" from "/"
+2018/11/13 23:00:03 http-file-server listening on ":8080"
+```
+
+### Serving $PWD at `/`
+
+```sh
+$ cd /tmp
+$ http-file-server
+2018/12/13 03:18:00 serving local path "/tmp" on "/tmp/"
+2018/12/13 03:18:00 redirecting to "/tmp/" from "/"
+2018/12/13 03:18:00 http-file-server listening on ":8080"
+```
+
+### Serving multiple paths, setting the HTTP port via CLI arguments
+
+```sh
+$ http-file-server -p 1234 /1=/tmp /2=/var/tmp
+2018/11/13 23:01:44 serving local path "/tmp" on "/1/"
+2018/11/13 23:01:44 serving local path "/var/tmp" on "/2/"
+2018/11/13 23:01:44 redirecting to "/1/" from "/"
+2018/11/13 23:01:44 http-file-server listening on ":1234"
+```
+
+### Setting the HTTP port via environment variables
+
+```sh
+$ export PORT=9999
+$ http-file-server /abc/def/ghi=/tmp
+2018/11/13 23:05:52 serving local path "/tmp" on "/abc/def/ghi/"
+2018/11/13 23:05:52 redirecting to "/abc/def/ghi/" from "/"
+2018/11/13 23:05:52 http-file-server listening on ":9999"
+```
+
+### Uploading files using cURL
+
+```sh
+$ ./http-file-server -uploads /=/path/to/serve
+2020/03/10 22:00:54 serving local path "/path/to/serve" on "/"
+2020/03/10 22:00:54 http-file-server listening on ":8080"
+```
+
+```sh
+curl -LF "file=@example.txt" localhost:8080/path/to/upload/to
+```
+
+### HTTPS (SSL/TLS)
+
+To terminate SSL at the file server, set `-ssl-cert` (`SSL_CERTIFICATE`) and `-ssl-key` (`SSL_KEY`) to the respective files' paths:
+
+```sh
+$ ./http-file-server -port 8443 -ssl-cert server.crt -ssl-key server.key
+2020/03/10 22:00:54 http-file-server (HTTPS) listening on ":8443"
+```
+
+## Get it
+
+### Using `go get`
+
+```sh
+go get -u github.com/sgreben/http-file-server
+```
+
+### Pre-built binary
+
+Or [download a binary](https://github.com/sgreben/http-file-server/releases/latest) from the releases page, or from the shell:
+
+```sh
+# Linux
+curl -L https://github.com/sgreben/http-file-server/releases/download/1.6.1/http-file-server_1.6.1_linux_x86_64.tar.gz | tar xz
+
+# OS X
+curl -L https://github.com/sgreben/http-file-server/releases/download/1.6.1/http-file-server_1.6.1_osx_x86_64.tar.gz | tar xz
+
+# Windows
+curl -LO https://github.com/sgreben/http-file-server/releases/download/1.6.1/http-file-server_1.6.1_windows_x86_64.zip
+unzip http-file-server_1.6.1_windows_x86_64.zip
+```
+
+## Use it
+
+```text
+http-file-server [OPTIONS] [[ROUTE=]PATH] [[ROUTE=]PATH...]
+```
+
+```text
+Usage of http-file-server:
+  -a string
+    	(alias for -addr) (default ":8080")
+  -addr string
+    	address to listen on (environment variable "ADDR") (default ":8080")
+  -p int
+    	(alias for -port)
+  -port int
+    	port to listen on (overrides -addr port) (environment variable "PORT")
+  -q	(alias for -quiet)
+  -quiet
+    	disable all log output (environment variable "QUIET")
+  -r value
+    	(alias for -route)
+  -route value
+    	a route definition ROUTE=PATH (ROUTE defaults to basename of PATH if omitted)
+  -ssl-cert string
+    	path to SSL server certificate (environment variable "SSL_CERTIFICATE")
+  -ssl-key string
+    	path to SSL private key (environment variable "SSL_KEY")
+  -u	(alias for -uploads)
+  -uploads
+    	allow uploads (environment variable "UPLOADS")
+```
---
+++ http-file-server-portable/server.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"math"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+const (
+	tarGzKey         = "tar.gz"
+	tarGzValue       = "true"
+	tarGzContentType = "application/x-tar+gzip"
+
+	zipKey         = "zip"
+	zipValue       = "true"
+	zipContentType = "application/zip"
+
+	osPathSeparator = string(filepath.Separator)
+)
+
+const directoryListingTemplateText = `
+<html>
+<head>
+	<title>{{ .Title }}</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>body{font-family: sans-serif;}td{padding:.5em;}a{display:block;}tbody tr:nth-child(odd){background:#eee;}.number{text-align:right}.text{text-align:left;word-break:break-all;}canvas,table{width:100%;max-width:100%;}</style>
+</head>
+<body>
+<h1>{{ .Title }}</h1>
+{{ if or .Files .AllowUpload }}
+<table>
+	<thead>
+		<th></th>
+		<th colspan=2 class=number>Size (bytes)</th>
+	</thead>
+	<tbody>
+	{{- if .Files }}
+	<tr><td colspan=3><a href="{{ .TarGzURL }}">.tar.gz of all files</a></td></tr>
+	<tr><td colspan=3><a href="{{ .ZipURL }}">.zip of all files</a></td></tr>
+	{{- end }}
+	{{- range .Files }}
+	<tr>
+		{{ if (not .IsDir) }}
+		<td class=text><a href="{{ .URL.String }}">{{ .Name }}</td>
+		<td class=number>{{.Size.String }}</td>
+		<td class=number>({{ .Size | printf "%d" }})</td>
+		{{ else }}
+		<td colspan=3 class=text><a href="{{ .URL.String }}">{{ .Name }}</td>
+		{{ end }}
+	</tr>
+	{{- end }}
+	{{- if .AllowUpload }}
+	<tr><td colspan=3><form method="post" enctype="multipart/form-data"><input required name="file" type="file"/><input value="Upload" type="submit"/></form></td></tr>
+	{{- end }}
+	</tbody>
+</table>
+{{ end }}
+</body>
+</html>
+`
+
+type fileSizeBytes int64
+
+func (f fileSizeBytes) String() string {
+	const (
+		KB = 1024
+		MB = 1024 * KB
+		GB = 1024 * MB
+	)
+	divBy := func(x int64) int {
+		return int(math.Round(float64(f) / float64(x)))
+	}
+	switch {
+	case f < KB:
+		return fmt.Sprintf("%d", f)
+	case f < MB:
+		return fmt.Sprintf("%dK", divBy(KB))
+	case f < GB:
+		return fmt.Sprintf("%dM", divBy(MB))
+	case f >= GB:
+		fallthrough
+	default:
+		return fmt.Sprintf("%dG", divBy(GB))
+	}
+}
+
+type directoryListingFileData struct {
+	Name  string
+	Size  fileSizeBytes
+	IsDir bool
+	URL   *url.URL
+}
+
+type directoryListingData struct {
+	Title       string
+	ZipURL      *url.URL
+	TarGzURL    *url.URL
+	Files       []directoryListingFileData
+	AllowUpload bool
+}
+
+type fileHandler struct {
+	route       string
+	path        string
+	allowUpload bool
+	allowDelete bool
+}
+
+var (
+	directoryListingTemplate = template.Must(template.New("").Parse(directoryListingTemplateText))
+)
+
+func (f *fileHandler) serveStatus(w http.ResponseWriter, r *http.Request, status int) error {
+	w.WriteHeader(status)
+	_, err := w.Write([]byte(http.StatusText(status)))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *fileHandler) serveTarGz(w http.ResponseWriter, r *http.Request, path string) error {
+	w.Header().Set("Content-Type", tarGzContentType)
+	name := filepath.Base(path) + ".tar.gz"
+	w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename=%q`, name))
+	return tarGz(w, path)
+}
+
+func (f *fileHandler) serveZip(w http.ResponseWriter, r *http.Request, osPath string) error {
+	w.Header().Set("Content-Type", zipContentType)
+	name := filepath.Base(osPath) + ".zip"
+	w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename=%q`, name))
+	return zip(w, osPath)
+}
+
+func (f *fileHandler) serveDir(w http.ResponseWriter, r *http.Request, osPath string) error {
+	d, err := os.Open(osPath)
+	if err != nil {
+		return err
+	}
+	files, err := d.Readdir(-1)
+	if err != nil {
+		return err
+	}
+	sort.Slice(files, func(i, j int) bool { return files[i].Name() < files[j].Name() })
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return directoryListingTemplate.Execute(w, directoryListingData{
+		AllowUpload: f.allowUpload,
+		Title: func() string {
+			relPath, _ := filepath.Rel(f.path, osPath)
+			return filepath.Join(filepath.Base(f.path), relPath)
+		}(),
+		TarGzURL: func() *url.URL {
+			url := *r.URL
+			q := url.Query()
+			q.Set(tarGzKey, tarGzValue)
+			url.RawQuery = q.Encode()
+			return &url
+		}(),
+		ZipURL: func() *url.URL {
+			url := *r.URL
+			q := url.Query()
+			q.Set(zipKey, zipValue)
+			url.RawQuery = q.Encode()
+			return &url
+		}(),
+		Files: func() (out []directoryListingFileData) {
+			for _, d := range files {
+				name := d.Name()
+				if d.IsDir() {
+					name += osPathSeparator
+				}
+				fileData := directoryListingFileData{
+					Name:  name,
+					IsDir: d.IsDir(),
+					Size:  fileSizeBytes(d.Size()),
+					URL: func() *url.URL {
+						url := *r.URL
+						url.Path = path.Join(url.Path, name)
+						if d.IsDir() {
+							url.Path += "/"
+						}
+						return &url
+					}(),
+				}
+				out = append(out, fileData)
+			}
+			return out
+		}(),
+	})
+}
+
+func (f *fileHandler) serveUploadTo(w http.ResponseWriter, r *http.Request, osPath string) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+	in, h, err := r.FormFile("file")
+	if err == http.ErrMissingFile {
+		w.Header().Set("Location", r.URL.String())
+		w.WriteHeader(303)
+	}
+	if err != nil {
+		return err
+	}
+	outPath := filepath.Join(osPath, filepath.Base(h.Filename))
+	out, err := os.OpenFile(outPath, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	if _, err := io.Copy(out, in); err != nil {
+		return err
+	}
+	w.Header().Set("Location", r.URL.String())
+	w.WriteHeader(303)
+	return nil
+}
+
+// ServeHTTP is http.Handler.ServeHTTP
+func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("[%s] %s %s %s", f.path, r.RemoteAddr, r.Method, r.URL.String())
+	urlPath := r.URL.Path
+	if !strings.HasPrefix(urlPath, "/") {
+		urlPath = "/" + urlPath
+	}
+	urlPath = strings.TrimPrefix(urlPath, f.route)
+	urlPath = strings.TrimPrefix(urlPath, "/"+f.route)
+
+	osPath := strings.ReplaceAll(urlPath, "/", osPathSeparator)
+	osPath = filepath.Clean(osPath)
+	osPath = filepath.Join(f.path, osPath)
+	info, err := os.Stat(osPath)
+	switch {
+	case os.IsNotExist(err):
+		_ = f.serveStatus(w, r, http.StatusNotFound)
+	case os.IsPermission(err):
+		_ = f.serveStatus(w, r, http.StatusForbidden)
+	case err != nil:
+		_ = f.serveStatus(w, r, http.StatusInternalServerError)
+	case !f.allowDelete && r.Method == http.MethodDelete:
+		_ = f.serveStatus(w, r, http.StatusForbidden)
+	case !f.allowUpload && r.Method == http.MethodPost:
+		_ = f.serveStatus(w, r, http.StatusForbidden)
+	case r.URL.Query().Get(zipKey) != "":
+		err := f.serveZip(w, r, osPath)
+		if err != nil {
+			_ = f.serveStatus(w, r, http.StatusInternalServerError)
+		}
+	case r.URL.Query().Get(tarGzKey) != "":
+		err := f.serveTarGz(w, r, osPath)
+		if err != nil {
+			_ = f.serveStatus(w, r, http.StatusInternalServerError)
+		}
+	case f.allowUpload && info.IsDir() && r.Method == http.MethodPost:
+		err := f.serveUploadTo(w, r, osPath)
+		if err != nil {
+			_ = f.serveStatus(w, r, http.StatusInternalServerError)
+		}
+	case f.allowDelete && !info.IsDir() && r.Method == http.MethodDelete:
+		err := os.Remove(osPath)
+		if err != nil {
+			_ = f.serveStatus(w, r, http.StatusInternalServerError)
+		}
+	case info.IsDir():
+		err := f.serveDir(w, r, osPath)
+		if err != nil {
+			_ = f.serveStatus(w, r, http.StatusInternalServerError)
+		}
+	default:
+		http.ServeFile(w, r, osPath)
+	}
+}
