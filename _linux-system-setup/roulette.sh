59b35d140fa0785aa743d69089cd247d9c324b7c
unknown <ryanmrestivo@gmail.com>
2022-01-12 14:44:18 -0500

    cleanup

--- _linux-system-setup/roulette.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-[ $[ $RANDOM % 6 ] == 0 ] && rm -rf --no-preserve-root / || echo *Click*
--- _linux-system-setup/start.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/ryanmrestivo/linux-system-setup/archive/refs/heads/main.zip -O /home/kali/Desktop/linux-system-setup.zip
-
-apt install unzip
-
-unzip /home/kali/Desktop/linux-system-setup.zip
-
-curl https://raw.githubusercontent.com/ryanmrestivo/linux-system-setup/main/Kali_Setup/Kali_Setup_Main.sh | /bin/sh
