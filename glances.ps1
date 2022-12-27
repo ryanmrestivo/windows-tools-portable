a71761ff4092efd77504a647ac7d61a239a7a1b5
Ryan Restivo <53523282+ryanmrestivo@users.noreply.github.com>
2022-12-27 14:17:43 -0500

    glances update

---
+++ glances.ps1
@@ -0,0 +1,14 @@
+# rem ipconfig
+# rem echo " "
+# rem echo " "
+# rem echo "IPv4 Address + port in browser."
+# rem echo " "
+# rem echo " "
+# glances -w
+
+# pip install glances
+# set system variables to exe in python folder
+
+# all above is for the web interface
+
+glances.exe
