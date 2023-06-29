0be0d7cdc32b31d0d180c384f2ceb0504dade619
ryanmrestivo <ryanmrestivo@gmail.com>
2023-06-29 16:43:42 -0400

    Update GoToUEFI.bat

--- Utilities/GoToUEFI.bat
+++ Utilities/GoToUEFI.bat
@@ -1,1 +1,12 @@
-shutdown /r /fw
+@echo off
+REM This script restarts the computer and enters the firmware configuration interface (UEFI).
+
+REM Ask the user for confirmation
+set /p UserInput="Are you sure you want to restart and enter UEFI? All unsaved work will be lost. (Y/N): "
+if /i "%UserInput%" neq "Y" goto :eof
+
+REM Specify a timeout (in seconds) before the computer restarts
+set Timeout=10
+
+echo The computer will restart in %Timeout% seconds...
+shutdown /r /fw /t %Timeout%
