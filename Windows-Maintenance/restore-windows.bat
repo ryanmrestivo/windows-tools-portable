848dc146d8f527e1a9711a20914ccf423ebfca78
ryanmrestivo <ryanmrestivo@gmail.com>
2023-06-29 16:34:15 -0400

    Update restore-windows.bat

--- Windows-Maintenance/restore-windows.bat
+++ Windows-Maintenance/restore-windows.bat
@@ -1,3 +1,23 @@
-:: If this script isn't working, run in PowerShell.
+@echo off
+REM This script repairs system files using sfc and DISM
 
-sfc /scannow && DISM.exe /Online /Cleanup-image /Scanhealth && DISM.exe /Online /Cleanup-image /Restorehealth
+REM Set the log file path to the same directory as the script
+set "LOGFILE=%~dp0RepairLog.txt"
+
+echo Starting System Repair Script at %date% %time% > "%LOGFILE%"
+
+echo Running System File Checker. Please wait...
+echo [%date% %time%] Running System File Checker >> "%LOGFILE%"
+sfc /scannow >> "%LOGFILE%" 2>&1
+
+echo Running DISM ScanHealth. Please wait...
+echo [%date% %time%] Running DISM ScanHealth >> "%LOGFILE%"
+DISM.exe /Online /Cleanup-image /Scanhealth >> "%LOGFILE%" 2>&1
+
+echo Running DISM RestoreHealth. Please wait...
+echo [%date% %time%] Running DISM RestoreHealth >> "%LOGFILE%"
+DISM.exe /Online /Cleanup-image /Restorehealth >> "%LOGFILE%" 2>&1
+
+echo Script completed at %date% %time%. Check the log file for details.
+echo Script completed at %date% %time% >> "%LOGFILE%"
+pause >nul
