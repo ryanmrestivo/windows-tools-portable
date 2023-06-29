b5ea9dff7b6dde43f2660d25bfd360326d9b4dc0
ryanmrestivo <ryanmrestivo@gmail.com>
2023-06-29 12:30:47 -0400

    Update choco-setup.ps1

--- Windows-Setup/choco-setup.ps1
+++ Windows-Setup/choco-setup.ps1
@@ -6,87 +6,130 @@
 
 # NOTE:  Run this script before doing any hardening or these installs will take substantially longer
 
+
 Set-ExecutionPolicy unrestricted -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # refreshenv
 	refreshenv
-	
-# winget
-	choco install winget
-	
-	winget install JanDeDobbeleer.OhMyPosh -s winget
-	
-	oh-my-posh init pwsh --config 'C:\Users\admin\AppData\Local\Programs\oh-my-posh\themes\montys.omp.json' | Invoke-Expression
-	New-Item -Path $PROFILE -Type File -Force
-	
-#Choco Applications
-#https://chocolatey.org/install
-    choco install 7zip.install
-    choco install adobereader
-#	 choco install cmake
-	choco install cmder
-    choco install curl
-    choco install discord.install
-	choco install docker-desktop
-    choco install dotnetfx
-	choco install ecm
-    choco install everything
-    choco install firefox
-    choco install geforce-experience
-    choco install gh
-    choco install git.install
-    choco install github-desktop
-    choco install git-lfs
-#	 choco install golang
-    choco install googlechrome
-    choco install greenshot
-    choco install hosts.editor
-    choco install javaruntime
-    choco install jdk11
-    choco install jre8
-    choco install lockhunter
-    choco install microsoft-edge
-#	 choco install mingw
-    choco install nmap
-    choco install nodejs.install
-    choco install nomacs
-    choco install notepadplusplus.install
-#	choco install ntop.portable
-	choco install onionshare
-    choco install openjdk 
-    choco install openjdk11
-#    choco install plasso
-    choco install powershellhere-elevated 
-    choco install processhacker
-    choco install protonvpn
-	choco install python
-#    choco install python2
-#    choco install python3
-	choco install pip
-    choco install qbittorrent
-#    choco install r.project
-	 choco install rustdesk
-#    choco install sudo
-#	choco install tailscale
-    choco install tor-browser
-#    choco install veracrypt
-    choco install vlc
-    choco install wget
-#    choco install wireshark
-#    choco install zap
-    choco install zoom
-    choco install visualstudio-installer
-    choco install visualstudio2022community
-#    choco install visualstudio2017-powershelltools
-#    choco install vscode-ruby vscodium vscode-ansible vscode-python chocolatey-vscode vscode-prettier vscode-java vscode-yaml vscode-haskell vscode-mongo vscode-beautify vscode-intellicode vscode-pull-request-github vscode-kubernetes-tools vscode-autofilename vscode-codespellchecker vscode-icons vscode-csharp dsc.powershellcommunity
 
-	pip install poetry
-	pip install windows-curses
-	pip install glances
+# Set PowerShell execution policy for the current process to unrestricted
+Set-ExecutionPolicy unrestricted -Scope Process -Force
+
+# Set Security Protocol
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+
+# Function to install a Chocolatey package and validate installation
+function Install-ChocoPackage {
+    param (
+        [string]$packageName
+    )
+    try {
+        # Installing Chocolatey package
+        Write-Host "Installing $packageName..."
+        choco install $packageName -y
+        # Validating installation
+        if (Get-Command $packageName -ErrorAction SilentlyContinue) {
+            Write-Host "$packageName installed successfully."
+        } else {
+            Write-Host "Unable to validate installation of $packageName."
+        }
+    } catch {
+        Write-Host "Error occurred while installing $packageName."
+    }
+}
+
+# Installing Chocolatey if not already installed
+try {
+    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+} catch {
+    Write-Host "Error occurred while installing Chocolatey."
+}
+
+# Refresh environment variables
+refreshenv
+
+# Install winget using Chocolatey
+Install-ChocoPackage -packageName "winget"
+
+# Install Oh-My-Posh using winget and setup the configuration
+try {
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+    oh-my-posh init pwsh --config 'C:\Users\admin\AppData\Local\Programs\oh-my-posh\themes\montys.omp.json' | Invoke-Expression
+    New-Item -Path $PROFILE -Type File -Force
+} catch {
+    Write-Host "Error occurred while installing Oh-My-Posh."
+}
+
+# List of Chocolatey packages to install
+$chocoPackages = @(
+    '7zip.install',
+    'adobereader',
+    'cmder',
+    'curl',
+    'discord.install',
+    'docker-desktop',
+    'dotnetfx',
+    'ecm',
+    'everything',
+    'firefox',
+    'geforce-experience',
+    'gh',
+    'git.install',
+    'github-desktop',
+    'git-lfs',
+    'googlechrome',
+    'greenshot',
+    'hosts.editor',
+    'javaruntime',
+    'jdk11',
+    'jre8',
+    'lockhunter',
+    'microsoft-edge',
+    'nmap',
+    'nodejs.install',
+    'nomacs',
+    'notepadplusplus.install',
+    'onionshare',
+    'openjdk',
+    'openjdk11',
+    'powershellhere-elevated',
+    'processhacker',
+    'protonvpn',
+    'python',
+    'pip',
+    'qbittorrent',
+    'rustdesk',
+    'tor-browser',
+    'vlc',
+    'wget',
+    'zoom',
+    'visualstudio-installer',
+    'visualstudio2022community'
+)
+
+# Install each Chocolatey package from the list
+foreach ($package in $chocoPackages) {
+    Install-ChocoPackage -packageName $package
+}
+
+# Install additional Python packages using pip
+try {
+    pip install poetry
+    pip install windows-curses
+    pip install glances
+} catch {
+    Write-Host "Error occurred while installing Python packages."
+}
 
 # Windows Update
 
-Install-Module -Name PSWindowsUpdate -Force
-Get-Package -Name PSWindowsUpdate
-get-command -module PSWindowsUpdate
-Get-WindowsUpdate
-Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+# Install and configure PSWindowsUpdate module for managing Windows updates
+try {
+    Install-Module -Name PSWindowsUpdate -Force
+    Get-Package -Name PSWindowsUpdate
+    get-command -module PSWindowsUpdate
+    # Fetch and install available Windows updates
+    Get-WindowsUpdate
+    Get-WindowsUpdate -AcceptAll -Install -AutoReboot
+} catch {
+    Write-Host "Error occurred while handling Windows updates."
+}
