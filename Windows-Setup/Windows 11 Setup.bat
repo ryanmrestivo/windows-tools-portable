b571fcd0ebc8cc9cdd64d6ba469d3b5c092da137
ryanmrestivo <ryanmrestivo@gmail.com>
2023-06-29 12:54:26 -0400

    Update Windows 11 Setup.bat

--- Windows-Setup/Windows 11 Setup.bat
+++ Windows-Setup/Windows 11 Setup.bat
@@ -9,181 +9,69 @@ echo.
 echo.                                                                                                                                                      
 echo  _____________________________________________________________________________________________________________________________________________________
 echo.
-echo  [1] ^| Debloat Everything
+echo  [1] ^| Debloat Windows 11
+echo.
+echo  This script is designed to remove unnecessary built-in applications and services from Windows 11 to optimize performance.
+echo  Below is the list of actions that will be performed:
+echo.
+echo  - Remove specified built-in applications (e.g., YourPhone, Zune, OneNote).
+echo  - Uninstall Microsoft Edge.
+echo  - Remove Windows Media Player.
+echo  - Disable scheduled tasks for Maps updates.
+echo  - Remove Quick Assist and Windows Hello Face.
+echo  - Uninstall and remove OneDrive.
+echo.
+echo  Press '1' and Enter to proceed with the debloat process, or press '0' and Enter to exit.
 echo.
 
 set /p op=  Your Option:
 if %op%==1 goto Op1
-if %op%==2 goto Op3
-if %op%==0 goto Op3
+if %op%==0 goto End
 
 :Op1
 cls
 echo.
 echo.                                                                                                                                                                                                                                                                                                                
 echo  _____________________________________________________________________________________________________________________________________________________                                                                                                                                                        
-echo. 
+echo.
 
-powershell -command "Get-AppxPackage -allusers Microsoft.549981C3F5F10 | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.YourPhone* | Remove-AppxPackage"
+setlocal enabledelayedexpansion
+set "powershellCmd=powershell -command"
+set "appsToRemove=Microsoft.549981C3F5F10, *Microsoft.YourPhone*, *zune*, *sticky*, *maps*, *people*, *comm*, *mess*, *onenote*, *photo*, *bing*, *soundrec*, *Microsoft.Getstarted*, *Microsoft.WindowsFeedbackHub*, *Microsoft.3DBuilder*, *GetHelp*, *phone*, *Microsoft.Office.Sway*, *Microsoft.Office.Desktop*, *Microsoft.MicrosoftEdge.Stable*, *Microsoft.MicrosoftSolitaireCollection*, *Microsoft.Wallet*, *Microsoft.MixedReality.Portal*, *Microsoft.MicrosoftOfficeHub*, *Microsoft.ScreenSketch*"
+
+for %%a in (%appsToRemove%) do (
+    %powershellCmd% "Get-AppxPackage -AllUsers %%a | Remove-AppxPackage"
+)
+
 cd %PROGRAMFILES(X86)%\Microsoft\Edge\Application\9*\Installer && setup --uninstall --force-uninstall --system-level
 C:\Windows\install_wim_tweak.exe /o /l
 C:\Windows\install_wim_tweak.exe /o /c "Microsoft-Windows-Internet-Browser-Package" /r
 C:\Windows\install_wim_tweak.exe /h /o /l
-powershell -command "Get-AppxPackage -AllUsers *zune* | Remove-AppxPackage"
-powershell -command "Get-WindowsPackage -Online | Where PackageName -like *MediaPlayer* | Remove-WindowsPackage -Online -NoRestart"
-powershell -command "Get-AppxPackage -AllUsers *sticky* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *maps* | Remove-AppxPackage"
+
+%powershellCmd% "Get-WindowsPackage -Online | Where PackageName -like *MediaPlayer* | Remove-WindowsPackage -Online -NoRestart"
+
 schtasks /Change /TN "\Microsoft\Windows\Maps\MapsUpdateTask" /disable
 schtasks /Change /TN "\Microsoft\Windows\Maps\MapsToastTask" /disable
-powershell -command "Get-AppxPackage -AllUsers *people* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *comm* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *mess* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *onenote* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *photo* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *bing* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *soundrec* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.WindowsFeedbackHub* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.3DBuilder* | Remove-AppxPackage"
-powershell -command "Get-AppxPackage -AllUsers *GetHelp* | Remove-AppxPackage"
-powershell -command "Get-WindowsPackage -Online | Where PackageName -like *QuickAssist* | Remove-WindowsPackage -Online -NoRestart"
-powershell -command "Get-AppxPackage -AllUsers *phone* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.Office.Sway* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.Office.Desktop* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.MicrosoftEdge.Stable* | Remove-AppxPackage"
-powershell -command "Get-WindowsPackage -Online | Where PackageName -like *Hello-Face* | Remove-WindowsPackage -Online -NoRestart"
+
+%powershellCmd% "Get-WindowsPackage -Online | Where PackageName -like *QuickAssist* | Remove-WindowsPackage -Online -NoRestart"
+%powershellCmd% "Get-WindowsPackage -Online | Where PackageName -like *Hello-Face* | Remove-WindowsPackage -Online -NoRestart"
+
 schtasks /Change /TN "\Microsoft\Windows\HelloFace\FODCleanupTask" /Disable
+
 %SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
 rd "%UserProfile%\OneDrive" /s /q
 rd "%LocalAppData%\Microsoft\OneDrive" /s /q
 rd "%ProgramData%\Microsoft OneDrive" /s /q
 rd "C:\OneDriveTemp" /s /q
-del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" /s /f /q
-powerShell -Command "Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.Wallet* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.MixedReality.Portal* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage"
-powerShell -Command "Get-AppxPackage *Microsoft.ScreenSketch* | Remove-AppxPackage"
-for /f "tokens=1* delims=" %I in (' reg query "HKEY_CLASSES_ROOT\SystemFileAssociations" /s /k /f "3D Edit" ^| find /i "3D Edit" ') do (reg delete "%I" /f )
-for /f "tokens=1* delims=" %I in (' reg query "HKEY_CLASSES_ROOT\SystemFileAssociations" /s /k /f "3D Print" ^| find /i "3D Print" ') do (reg delete "%I" /f )
 
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0 /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules"  /v "{2765E0F4-2918-4A46-B9C9-43CDD8FCBA2B}" /t REG_SZ /d  "BlockCortana|Action=Block|Active=TRUE|Dir=Out|App=C:\windows\systemapps\microsoft.windows.cortana_cw5n1h2txyewy\searchui.exe|Name=Search  and Cortana  application|AppPkgId=S-1-15-2-1861897761-1695161497-2927542615-642690995-327840285-2659745135-2630312742|" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f
+rem Log completion and any errors to files
+echo Completed debloating process >> log.txt
+2>> error.log
 
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Error Reporting" /v Disabled /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v Disabled /t REG_DWORD /d 1 /f
-
-
-sc delete DiagTrack
-sc delete dmwappushservice
-sc delete WerSvc
-sc delete OneSyncSvc
-sc delete MessagingService
-sc delete wercplsupport
-sc delete PcaSvc
-sc config wlidsvc start=demand
-sc delete wisvc
-sc delete RetailDemo
-sc delete diagsvc
-sc delete shpamsvc 
-sc delete TermService
-sc delete UmRdpService
-sc delete SessionEnv
-sc delete TroubleshootingSvc
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "wscsvc" ^| find /i "wscsvc"') do (reg delete %I /f)
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "OneSyncSvc" ^| find /i "OneSyncSvc"') do (reg delete %I /f)
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "MessagingService" ^| find /i "MessagingService"') do (reg delete %I /f)
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "PimIndexMaintenanceSvc" ^| find /i "PimIndexMaintenanceSvc"') do (reg delete %I /f)
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "UserDataSvc" ^| find /i "UserDataSvc"') do (reg delete %I /f)
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "UnistoreSvc" ^| find /i "UnistoreSvc"') do (reg delete %I /f)
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "BcastDVRUserService" ^| find /i "BcastDVRUserService"') do (reg delete %I /f)
-for /f "tokens=1" %I in ('reg query "HKLM\SYSTEM\CurrentControlSet\Services" /k /f "Sgrmbroker" ^| find /i "Sgrmbroker"') do (reg delete %I /f)
-sc delete diagnosticshub.standardcollector.service
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Siuf\Rules" /v "NumberOfSIUFInPeriod" /t REG_DWORD /d 0 /f
-reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Siuf\Rules" /v "PeriodInNanoSeconds" /f
-reg add "HKLM\SYSTEM\ControlSet001\Control\WMI\AutoLogger\AutoLogger-Diagtrack-Listener" /v Start /t REG_DWORD /d 0 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v AITEnable /t REG_DWORD /d 0 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v DisableInventory /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v DisablePCA /t REG_DWORD /d 1 /f
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v DisableUAR /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" /v "EnabledV9" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableSmartScreen" /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Internet Explorer\PhishingFilter" /v "EnabledV9" /t REG_DWORD /d 0 /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoRecentDocsHistory" /t REG_DWORD /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\CompatTelRunner.exe" /v Debugger /t REG_SZ /d "%windir%\System32\taskkill.exe" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\DeviceCensus.exe" /v Debugger /t REG_SZ /d "%windir%\System32\taskkill.exe" /f
-
-schtasks /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /disable
-schtasks /Change /TN "Microsoft\Windows\Application Experience\AitAgent" /disable
-schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /disable
-schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /disable
-schtasks /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /disable
-schtasks /Change /TN "Microsoft\Windows\Autochk\Proxy" /disable
-schtasks /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\BthSQM" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Uploader" /disable
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /disable
-schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /disable
-schtasks /Change /TN "Microsoft\Windows\DiskFootprint\Diagnostics" /disable
-schtasks /Change /TN "Microsoft\Windows\FileHistory\File History (maintenance mode)" /disable
-schtasks /Change /TN "Microsoft\Windows\Maintenance\WinSAT" /disable
-schtasks /Change /TN "Microsoft\Windows\PI\Sqm-Tasks" /disable
-schtasks /Change /TN "Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /disable
-schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyMonitor" /disable
-schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyRefresh" /disable
-schtasks /Change /TN "Microsoft\Windows\Shell\FamilySafetyUpload" /disable
-schtasks /Change /TN "Microsoft\Windows\Windows Error Reporting\QueueReporting" /disable
-schtasks /Change /TN "Microsoft\Windows\WindowsUpdate\Automatic App Update" /disable
-schtasks /Change /TN "Microsoft\Windows\License Manager\TempSignedLicenseExchange" /disable
-schtasks /Change /TN "Microsoft\Windows\Clip\License Validation" /disable
-schtasks /Change /TN "\Microsoft\Windows\ApplicationData\DsSvcCleanup" /disable
-schtasks /Change /TN "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem" /disable
-schtasks /Change /TN "\Microsoft\Windows\PushToInstall\LoginCheck" /disable
-schtasks /Change /TN "\Microsoft\Windows\PushToInstall\Registration" /disable
-schtasks /Change /TN "\Microsoft\Windows\Shell\FamilySafetyMonitor" /disable
-schtasks /Change /TN "\Microsoft\Windows\Shell\FamilySafetyMonitorToastTask" /disable
-schtasks /Change /TN "\Microsoft\Windows\Shell\FamilySafetyRefreshTask" /disable
-schtasks /Change /TN "\Microsoft\Windows\Subscription\EnableLicenseAcquisition" /disable
-schtasks /Change /TN "\Microsoft\Windows\Subscription\LicenseAcquisition" /disable
-schtasks /Change /TN "\Microsoft\Windows\Diagnosis\RecommendedTroubleshootingScanner" /disable
-schtasks /Change /TN "\Microsoft\Windows\Diagnosis\Scheduled" /disable
-schtasks /Change /TN "\Microsoft\Windows\NetTrace\GatherNetworkInfo" /disable
-del /F /Q "C:\Windows\System32\Tasks\Microsoft\Windows\SettingSync\*" 
-
-REG ADD “HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked” /v {776DBC8D-7347-478C-8D71-791E12EF49D8} /d Skype
-
-goto done
-
-:done
+:End
 cls
-echo.                                                                                                                                                        
-echo.                                                                                                                                                           
-echo  _____________________________________________________________________________________________________________________________________________________                                                                                                                                                         
-echo. 
 echo.
-echo  ============================================================================================================
-echo         CONFIGURATION COMPLETE (RESTART REQUIRED! 
-echo  ============================================================================================================
-ping 127.0.0.1 >nul
-goto Start
-
-:Op3
-cls
-echo ========
-ping 127.0.0.1 >nul
-goto Start
-
-:input
-cls                                                                                                                                                       
-echo.                                                                                                                                                           
-echo  _____________________________________________________________________________________________________________________________________________________                                                                                                                                                      
-echo. 
-echo  ===================================================
-echo  # INVALID OPTION PLEASE CHOOSE THE CORRECT OPTION #
-echo  ===================================================
-ping localhost -n 4 >nul
-goto start
+echo Operation completed successfully!
+echo Press any key to exit...
+pause >nul
+exit
